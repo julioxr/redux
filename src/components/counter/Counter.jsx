b391cfb714cd6d07b2e2ff79d44785ee1858e882
julio <julioroja987@gmail.com>
2023-01-04 21:51:46 -0300

    primero

---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Counter = () => {
+    return (
+        <div>
+            <h1>Amount</h1>
+            <button>Deposit $10</button>
+            <button>Withdraw $10</button>
+        </div>
+    );
+};
+
+export default Counter;
